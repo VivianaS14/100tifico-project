6a26a03c1df70df30b2ad98cb7696d4c9bdcf7e1
VivianaS14 <vasorianp@gmail.com>
2022-12-08 22:41:33 -0500

    Trayendo el detalle de cada personaje

--- src/pages/character.js
+++ src/pages/character.js
@@ -1,17 +1,22 @@
-const character = () => {
+import getHash from '../utils/getHash';
+import getData from '../utils/getData';
+
+const character = async () => {
+    const id = getHash();
+    const character = await getData(id);
     const view = `
         <div class="characters-inner">
             <article class="characters-card">
-                <img src="image" alt="name">
-                <h2>Name</h2>
+                <img src="${character.data.image}" alt="name">
+                <h2>${character.data.name}</h2>
             </article>
             <article class="characters-card">
-                <h3>Episodes:</h3>
-                <h3>Status:</h3>
-                <h3>Species:</h3>
-                <h3>Gender:</h3>
-                <h3>Origin:</h3>
-                <h3>Last Location:</h3>
+                <h3>Episodes: <span>${character.data.episode.length}</span></h3>
+                <h3>Status: <span>${character.data.status}</span></h3>
+                <h3>Species: <span>${character.data.species}</span></h3>
+                <h3>Gender: <span>${character.data.gender}</span></h3>
+                <h3>Origin: <span>${character.data.origin.name}</span></h3>
+                <h3>Last Location: ${character.data.location.name}</h3>
             </article>
         </div>
     `
